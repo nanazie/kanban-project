af512000c9819a2b6ca9c2932329dd76157c907f
Naufal adil Pratama <naufaladilpratama3@gmail.com>
2023-11-14 11:27:27 +0700

    Develop home view

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+
+    return view('home'); // Diperbarui
+
+});
+
+Route::get('/', function () {
+
+    return view('home'); // Diperbarui
+
+});
+
+
